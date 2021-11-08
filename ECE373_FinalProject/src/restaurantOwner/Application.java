2ce4c01cc136c10a5197b393597e2b3477d98d43
rober <rkupps18@email.arizona.edu>
2021-11-08 12:11:36 -0700

    Added Fields, Getters and Setters for class Application

--- ECE373_FinalProject/src/restaurantOwner/Application.java
+++ ECE373_FinalProject/src/restaurantOwner/Application.java
@@ -1,5 +1,28 @@
 package restaurantOwner;
 
+import java.util.ArrayList;
+
 public class Application {
 
+	//Fields 
+	private ArrayList<Client> clients = new ArrayList<Client>();
+	private ArrayList<Owner> owners = new ArrayList<Owner>();
+	private ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+	
+	
+	//Methods
+	public ArrayList<Client> getClients() {
+		return clients;
+	}
+
+	public ArrayList<Owner> getOwners() {
+		return owners;
+	}
+
+	public ArrayList<Restaurant> getRestaurants() {
+		return restaurants;
+	}
+	
+	
+	
 }
