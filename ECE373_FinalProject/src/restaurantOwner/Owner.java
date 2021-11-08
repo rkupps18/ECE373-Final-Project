283028bd2a5a1fbf2e8a423db9b607fd87f0fd80
rober <rkupps18@email.arizona.edu>
2021-11-08 11:48:04 -0700

    Added fields, getters and setters for owner class

--- ECE373_FinalProject/src/restaurantOwner/Owner.java
+++ ECE373_FinalProject/src/restaurantOwner/Owner.java
@@ -1,6 +1,55 @@
 package restaurantOwner;
 
 public class Owner {
-	private String ownerName;
+	
+	//Fields
+	private String name;
 	private String email;
+	private String phone;
+	private String password;
+	private Restaurant restaurantOwned;
+	
+	
+	
+	//Methods
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Restaurant getRestaurantOwned() {
+		return restaurantOwned;
+	}
+
+	public void setRestaurantOwned(Restaurant restaurantOwned) {
+		this.restaurantOwned = restaurantOwned;
+	}
+	
 }
