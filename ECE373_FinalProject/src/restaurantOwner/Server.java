b873aea70fe1d11e8b943ded1d3e3835d689ad05
rober <rkupps18@email.arizona.edu>
2021-11-08 11:52:34 -0700

    Added Fields, getters and setters for Server Class

--- ECE373_FinalProject/src/restaurantOwner/Server.java
+++ ECE373_FinalProject/src/restaurantOwner/Server.java
@@ -1,5 +1,25 @@
 package restaurantOwner;
 
+import java.util.ArrayList;
+
 public class Server {
+
+		//Fields
+		private int serverID;
+		private ArrayList<Table> tablesAssigned = new ArrayList<Table>();
+		
+		
+		//Methods
+		public int getServerID() {
+			return serverID;
+		}
+		
+		public void setServerID(int serverID) {
+			this.serverID = serverID;
+		}
+
+		public ArrayList<Table> getTablesAssigned() {
+			return tablesAssigned;
+		}
 
 }
