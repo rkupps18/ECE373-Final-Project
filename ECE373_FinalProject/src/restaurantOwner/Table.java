b6f56049d2ee4b20f42c727691cc9606724d10ec
rober <rkupps18@email.arizona.edu>
2021-11-08 12:07:33 -0700

    Added fields, getters and setters for class table

--- ECE373_FinalProject/src/restaurantOwner/Table.java
+++ ECE373_FinalProject/src/restaurantOwner/Table.java
@@ -1,5 +1,35 @@
 package restaurantOwner;
 
+import java.util.ArrayList;
+
 public class Table {
 
+	//Fields
+	private int tableNumber;
+	private int tableCapacity;
+	private ArrayList<Reservation> reservationsScheduled = new ArrayList<Reservation>();
+	
+	
+	//Methods
+	public int getTableNumber() {
+		return tableNumber;
+	}
+	
+	public void setTableNumber(int tableNumber) {
+		this.tableNumber = tableNumber;
+	}
+
+	public int getTableCapacity() {
+		return tableCapacity;
+	}
+
+	public void setTableCapacity(int tableCapacity) {
+		this.tableCapacity = tableCapacity;
+	}
+
+	public ArrayList<Reservation> getReservationsScheduled() {
+		return reservationsScheduled;
+	}
+	
+	
 }
