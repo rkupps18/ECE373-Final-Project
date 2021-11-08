02cef9192261b6e7fe33a3cb4f0634f41795f7fe
rober <rkupps18@email.arizona.edu>
2021-11-08 11:44:13 -0700

    Added fields and getter/setters for client class

--- ECE373_FinalProject/src/restaurantOwner/Client.java
+++ ECE373_FinalProject/src/restaurantOwner/Client.java
@@ -1,5 +1,47 @@
 package restaurantOwner;
 
+import java.util.ArrayList;
+
 public class Client {
+	
+	//Fields
 	private String name;
+	private String phone;
+	private String email;
+	private String password;
+	private ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+	
+	
+	//Methods
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
