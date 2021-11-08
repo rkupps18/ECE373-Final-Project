14a05551a08fbbc5a7e12286c32f778fb135b7b0
rober <rkupps18@email.arizona.edu>
2021-11-08 12:04:50 -0700

    Added fields, getters and setters for reservation

--- ECE373_FinalProject/src/restaurantOwner/Reservation.java
+++ ECE373_FinalProject/src/restaurantOwner/Reservation.java
@@ -1,5 +1,49 @@
 package restaurantOwner;
 
+import java.util.Date;
+
 public class Reservation {
 
+	//Fields
+	private Date day; //includes time to the milisecond too. Still need time var?
+	private int tableSize;
+	private Table tableAssigned;
+	private Client clientReserved;
+	
+	
+	//Methods
+	public Date getDay() {
+		return day;
+	}
+	
+	public void setDay(Date day) {
+		this.day = day;
+	}
+
+	public int getTableSize() {
+		return tableSize;
+	}
+
+	public void setTableSize(int tableSize) {
+		this.tableSize = tableSize;
+	}
+
+	public Table getTableAssigned() {
+		return tableAssigned;
+	}
+
+	public void setTableAssigned(Table tableAssigned) {
+		this.tableAssigned = tableAssigned;
+	}
+
+	public Client getClientReserved() {
+		return clientReserved;
+	}
+
+	public void setClientReserved(Client clientReserved) {
+		this.clientReserved = clientReserved;
+	}
+	
+	
+	
 }
