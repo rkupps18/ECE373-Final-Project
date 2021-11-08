1bb0f657ecba945d4e4811bbe308737e919fb79a
rober <rkupps18@email.arizona.edu>
2021-11-08 11:59:17 -0700

    Create fields, getters and setters for restaurant class

--- ECE373_FinalProject/src/restaurantOwner/Restaurant.java
+++ ECE373_FinalProject/src/restaurantOwner/Restaurant.java
@@ -1,5 +1,56 @@
 package restaurantOwner;
 
+import java.util.ArrayList;
+
 public class Restaurant {
+	
+	//Fields
+	private String name;
+	private String location;
+	//private image menu; // How tf do you add an image?
+	private Owner restaurantOwner;
+	private ArrayList<Server> servers = new ArrayList<Server>();
+	private ArrayList<Table> tables = new ArrayList<Table>();
+	private ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+
+	
+	
+	//Methods
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Owner getRestaurantOwner() {
+		return restaurantOwner;
+	}
+
+	public void setRestaurantOwner(Owner restaurantOwner) {
+		this.restaurantOwner = restaurantOwner;
+	}
+
+	public ArrayList<Server> getServers() {
+		return servers;
+	}
+
+	public ArrayList<Table> getTables() {
+		return tables;
+	}
+
+	public ArrayList<Reservation> getReservations() {
+		return reservations;
+	}	
+	
 
 }
