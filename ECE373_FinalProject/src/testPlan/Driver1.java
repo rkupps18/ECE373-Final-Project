d206a53174b156c43909707ad484f30d078071a7
rober <rkupps18@email.arizona.edu>
2021-11-11 16:15:27 -0700

    Added Driver file  in testPlan package for testing

---
+++ ECE373_FinalProject/src/testPlan/Driver1.java
@@ -0,0 +1,21 @@
+package testPlan;
+
+import restaurantOwner.Application;
+
+import restaurantOwner.Owner;
+
+public class Driver1 {
+	public static void main(String[] args) 
+	{
+		//Initialize the
+		Application ReservationsApp = new Application();
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
